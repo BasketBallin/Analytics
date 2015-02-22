afd99f2f281189aff50d7bbfbda3f01ec90cebd1
Austen Groener <rocketboyausten@gmail.com>
2015-02-22 14:41:35 -0500

    Added more to _get_boxscore and added the beginnings of display_boxscore.

--- scrape_core.py
+++ scrape_core.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
-import urllib2
+from tabulate import tabulate
+from urllib2 import urlopen
+
 import os.path #Can be removed once we set up the db
 import re
 import ipdb
@@ -45,7 +47,7 @@ class Scrape(object):
         self.game_url = self.base_url+"/leagues/NBA_"+str(self.year)+"_games.html"
 
         # Begin parsing
-        html = urllib2.urlopen(self.game_url).read()
+        html = urlopen(self.game_url).read()
         soup = BeautifulSoup(html, 'html.parser')
         table = soup.find('table', 'sortable stats_table')
         
@@ -60,18 +62,18 @@ class Scrape(object):
 	
         return boxscore_links
 
-    def _get_boxscore(self,game_url):
+    def _get_boxscore(self,game_url,field_type='basic'):
         """
         Private function which retrieves all data for a given game_url.
         """
         
         # Make sure that a game has been specified
         self.game_url = game_url
-        self.player_data = {}
+        self.boxscore_data = {}
         assert self.game_url is not None, "Must specify a game."
 
         # Setting up dictionary storage structure
-        data_fields = {'basic':['MP','FG','FGA','FG%','3P','3PA','3P%',
+        self.data_fields = {'basic':['MP','FG','FGA','FG%','3P','3PA','3P%',
                                 'FT','FTA','FT%','ORB','DRB','TRB','AST',
                                 'STL','BLK','TOV','PF','PTS','+/-'],
                        'advanced':['MP','TS%','eFG%','ORB%','DRB%','TRB%',
@@ -79,33 +81,59 @@ class Scrape(object):
                                    'DRtg']}
 
         # Obtain the html
-        html = urllib2.urlopen(self.game_url).read()
+        html = urlopen(self.game_url).read()
         gametag = self.game_url.split('/')[-1].strip('.html')
         soup = BeautifulSoup(html, 'lxml')
-        fields_basic = data_fields['basic']
+
+        fields = self.data_fields['{}'.format(field_type)]
         
         # Get table from html
-        tables = soup.findAll('table', id=re.compile('basic'))
+        tables = soup.findAll('table', id=re.compile(field_type))
         tables = [t.find('tbody') for t in tables]
+    
         for t in tables:
             data = t.findAll('tr')
             for player in data:
                 if player['class'][0] == '':
                     pdata = player.findAll('td')
                     name = pdata[0].string
-                    #if pdata[1].string != "Did Not Play":
-                    if len(pdata) > 2:
-                        tmp = player_data.get(name, {})
-                        for i,k in enumerate(fields_basic):
+                    # is this sufficient to always determine if
+                    # the player wasn't on the bench?
+                    if len(pdata) > 2: 
+                        tmp = self.boxscore_data.get(name, {})
+                        for i,k in enumerate(fields):
                             if pdata[i+1] == None:
                                 pdata[i+1] = 'None'
                             tmp[k] = pdata[i+1].string
-                        self.player_data[name] = tmp
+                        self.boxscore_data[name] = tmp
 
+        return self.boxscore_data
+
+    def display_boxscore(self,boxscore_data):
+        data = []
+
+        # headers
+        if len(boxscore_data[boxscore_data.keys()[0]]) == 20:
+            data.append(self.data_fields['basic'])
+        elif len(boxscore_data[boxscore_data.keys()[0]]) == 13:
+            data.append(self.data_fields['advanced'])
+        else:
+            "ERROR MESSAGE HERE"
+
+        # data
+        for player in self.boxscore_data:
+            row = [player]
+            for k in self.boxscore_data[player]:
+                row.append(self.boxscore_data[player][k])
+            data.append(row)
+        print tabulate(data)
+
+        
         
 if __name__ == "__main__":
     s2014 = Scrape(year=2014)
     links2014 = s2014._get_game_urls_for_season()
-    s2014._get_boxscore(links2014[0])
+    boxscore_data = s2014._get_boxscore(links2014[0])
+    s2014.display_boxscore(boxscore_data)
     ipdb.set_trace()
                             
